e3744d964bb88fdb8b1960b8f2897e75d887693e
Devon <devon_3273@hotmail.com>
2021-10-31 19:09:25 -0600

    initial commit, CLI app

---
+++ README.md
@@ -0,0 +1,8 @@
+# CLI Assignment
+- Just enter your preffered soda brand and flavour.
+- Missing info will return errors.
+- Incorrect inputs (Numbers) will return errors.
+--- 
+## Attributions
+- NPM Package: Chalk
+- Code References: https://github.com/sait-wbdv/in-class/blob/main/cpnt262/10-27-npm/01-spoilers-calculator-argv/app.js
---
+++ js/app.js
@@ -0,0 +1,33 @@
+const chalk = require('chalk')
+
+const arg = process.argv.slice(2)
+
+const pop = arg[0]
+const flavour = arg[1]
+// Enter your  preferred soda brand and flavour.
+// EXAMPLE: node app Coke Cherry || Orange Crush || Muggs Root-beer
+// ERROR EXAMPLE : node app 8 7 || node app Coke 8 || node app 8 Pepsi || node app || node app coke
+
+
+if (Number(pop) || Number(flavour)) {
+  console.log(chalk.red('Invalid entry. Please enter your preferred soda brand and flavour.'))
+} else {
+  if (!pop && !flavour) {
+    console.log(chalk.blue('Please enter your preferred soda brand and flavour.'))
+    }  else if (!pop) {
+      console.log('Please enter your preffered soda brand.')
+    } else if (!flavour){
+      console.log(chalk.blue('Please enter your preferred soda flavour.'))
+    } else if (pop && flavour){
+      console.log(chalk.green(`Well... ${pop}, ${flavour}. Not a bad choice!`))
+    } else {
+      console.log(chalk.red('Invalid entry. Please enter your preffered soda brand and flavour'))
+    }
+  }
+
+  
+
+
+
+
+ 
